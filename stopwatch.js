3d30c4966087d9324da1ac196b873b5351c4e7d5
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-01-02 14:32:00 +0100

    Update stopwatch.js
    Added const (3x)

--- stopwatch.js
+++ stopwatch.js
@@ -2,3 +2,7 @@ let hr = (min = sec = ms = "0" + 0),
   startTimer;
 
 
+const startBtn = document.querySelector(".start"),
+      stopBtn = document.querySelector(".stop"),
+      resetBtn = document.querySelector(".reset");
+
